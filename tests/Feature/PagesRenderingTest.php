bd35c5af9ea3555624d2d772668a08b6b8ec9de2
Bartek Woldański <bart@dev-1.pl>
2022-01-18 23:57:15 +0100

    test: routes & components rendering & admin routes

--- tests/Feature/PagesRenderingTest.php
+++ tests/Feature/PagesRenderingTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use ClaudioDekker\Inertia\Assert;
 use Tests\TestCase;
@@ -17,9 +18,40 @@ use function PHPUnit\Framework\assertEquals;
 
 class PagesRenderingTest extends TestCase
 {
+    use RefreshDatabase;
+
+
+    protected $fakeAdminUser;
+
+    public function properPageName(string $name):string
+    {
+        $explodeString = explode('-', $name);
+        $newArray = array_map('ucfirst', $explodeString);
+        return  implode($newArray);
+    }
+
+    public function createFakeAdmin():void
+    {
+        $this->fakeAdminUser = User::factory()->create([
+             'name' => 'bart-1',
+            'email' => 'studio@dziwnykot.pl',
+            'password' => bcrypt('1111'),
+            'phone' => '504399023',
+            'account_type' => 'business',
+            'company' => 'Dziwny Kot',
+            'nip' => 1192341997,
+            'street' => 'Budrysów',
+            'house_number' => '9',
+            'apartment_number' => '45',
+            'zip_code' => '00-119',
+            'city' => 'Warszawa',
+          'role' => 'admin'
+        ]);
+    }
+
     /**
-     * @dataProvider providePagesNames
-     */
+        * @dataProvider pages
+        */
 
     public function test_route_return_ok_status(string $dataProvider):void
     {
@@ -27,26 +59,49 @@ class PagesRenderingTest extends TestCase
         $response -> assertStatus(200);
     }
     /**
-     * @dataProvider providePagesNames
+        * @dataProvider adminPages
+        */
+
+    public function test_route_return_302_status_for_guest_user(string $dataProvider):void
+    {
+        $response = $this->get('/'.$dataProvider);
+        $response -> assertStatus(302);
+    }
+    /**
+        * @dataProvider adminPages
+        */
+
+    public function test_route_return_ok_status_for_admin_user(string $dataProvider):void
+    {
+        $this->createFakeAdmin();
+        $response = $this->actingAs($this->fakeAdminUser, 'web')->withSession(['banned' => false])->get('/'.$dataProvider);
+        $response -> assertStatus(200);
+    }
+    /**
+     * @dataProvider pages
      */
 
     public function test_inertia_can_render_page_by_route(string $dataProvider):void
     {
-        $response = $this->get('/'.$dataProvider);
-        $response->assertInertia(fn (Assert $page) => $page->url('/'.$dataProvider));
+        $findChar = strpos($dataProvider, '-') && $properComponentName = $this->properPageName($dataProvider);
+
+        if ($findChar) {
+            $response = $this->get('/'.$dataProvider);
+            $response->assertInertia(fn (Assert $page) => $page->url('/'.$dataProvider));
+        } else {
+            $response = $this->get('/'.$dataProvider);
+            $response->assertInertia(fn (Assert $page) => $page->url('/'.$dataProvider));
+        }
     }
 
     /**
-     * @dataProvider providePagesNames
+     * @dataProvider pages
      */
     public function test_inertia_can_find_component_by_route(string $dataProvider):void
     {
-        $findChar = strpos($dataProvider, '-');
+        $findChar = strpos($dataProvider, '-') && $properComponentName = $this->properPageName($dataProvider);
 
         if ($findChar) {
-            $explodeString = explode('-', $dataProvider);
-            $newArray = array_map('ucfirst', $explodeString);
-            $properComponentName = implode($newArray);
             $response = $this->get('/'.$dataProvider);
             $response->assertInertia(fn (Assert $page) => $page->component($properComponentName));
         } else {
@@ -55,15 +110,56 @@ class PagesRenderingTest extends TestCase
         }
     }
 
+    /**
+     * @dataProvider adminPages
+     */
 
-    public function providePagesNames():array
+    public function test_inertia_can_render_admin_pages_by_route(string $dataProvider):void
+    {
+        $this->createFakeAdmin();
+
+        $response = $this->actingAs($this->fakeAdminUser, 'web')->withSession(['banned' => false])->get('/'.$dataProvider);
+        $response->assertInertia(fn (Assert $page) => $page->url('/'.$dataProvider));
+    }
+
+    /**
+     * @dataProvider adminPages
+     */
+    public function test_inertia_can_find_admin_components_by_route(string $dataProvider):void
+    {
+        $this->createFakeAdmin();
+
+
+        $findChar = strpos($dataProvider, '-') && $properComponentName = $this->properPageName($dataProvider);
+
+
+        if ($findChar) {
+            $response = $this->actingAs($this->fakeAdminUser)->withSession(['banned' => false])->get('/'.$dataProvider);
+            $response->assertInertia(fn (Assert $page) => $page->component($properComponentName));
+        } else {
+            $response = $this->actingAs($this->fakeAdminUser)->withSession(['banned' => false])->get('/'.$dataProvider);
+            $response->assertInertia(fn (Assert $page) => $page->component(ucfirst(strval($dataProvider))));
+        }
+    }
+
+
+
+
+
+    public function pages():array
     {
         return [
-            'hello' => ['hello'],
+            'start' => ['start'],
             'printshop'=>['printshop'],
             'creator'=>['creator'],
             'contact'=>['contact'],
+        ];
+    }
+    public function adminPages():array
+    {
+        return [
             'admin-tools'=>['admin-tools'],
         ];
     }
 }
+;
