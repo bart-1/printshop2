e897f6ef59b754547ea13a04edcea9e41a479717
Bartosz Woldanski <studio@dziwnykot.pl>
2022-02-13 22:19:18 +0100

    fix: AddNewUserSeeder - change class name like file name

--- database/seeders/AddNewUserSeeder.php
+++ database/seeders/AddNewUserSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class AddNewUser extends Seeder
+class AddNewUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
